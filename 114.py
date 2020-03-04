e9fbb01e2ef8ae92cc5c2eeed296f96a4c30ce1e
wonszrzeczny <33986252+wonszrzeczny@users.noreply.github.com>
2020-03-04 23:49:07 +0100

    Revert "commit"
    This reverts commit cde1cb1d

--- 114.py
+++ 114.py
@@ -1,11 +1,9 @@
-m=50
+
 def t(n,vals={}):
     if n not in vals:
-        vals[n]=sum([t(k) for d in range(m,n+1) for k in range(m,n-d)])+n-(m-1)
+        vals[n]=sum([t(k) for d in range(3,n+1) for k in range(3,n-d)])+n-2
     return vals[n]
 def p(n):
-    return sum([t(p) for p in range(m,n+1)])+1
-i=51
-while(p(i)<10**6):
-    i+=1
-print(i)
+    return sum([t(p) for p in range(2,n+1)])+1
+
+print(p(50))
